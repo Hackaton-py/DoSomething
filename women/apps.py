663bfc50d6222821ec7ad96ce19f735a8504bf45
bael <bayelnurkamilov@gmail.com>
2023-06-05 13:58:05 +0600

    first commit

--- women/apps.py
+++ women/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DevnetworkConfig(AppConfig):
+class WomenConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DevNetwork'
+    name = 'women'
